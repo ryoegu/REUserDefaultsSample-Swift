02d4d05c362bda6380792a3710669202cade2710
Ryo Eguchi <ryo.egu@gmail.com>
2014-10-28 22:28:45 +0900

    Initial Commit

--- REUserDefaultsSample-Swift/SecondViewController.swift
+++ REUserDefaultsSample-Swift/SecondViewController.swift
@@ -9,27 +9,47 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var userProgressView: UIProgressView!
+    @IBOutlet var onoffLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-    }
+        
+        //NSUserDefaults（倉庫）からデータを取る
+        //（STEP.1）倉庫の作成
+        let savedata = NSUserDefaults.standardUserDefaults()
+        //（STEP.2）鍵を入れて、データをとってくる
+        userNameLabel.text = savedata.stringForKey("userName")
+        userProgressView.progress = savedata.floatForKey("slider")
 
+        if savedata.boolForKey("onoff") {
+            self.onoffLabel.text = "ON"
+        }else{
+            self.onoffLabel.text = "OFF"
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func backButton(sender: UIButton) {
+        self.dismissViewControllerAnimated(true,completion:nil);
+    }
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
--- REUserDefaultsSample-Swift/ViewController.swift
+++ REUserDefaultsSample-Swift/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var userSlider: UISlider!
+    @IBOutlet var onoffSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendButton(sender: UIButton) {
+        //NSUserDefaultsの公式を使おう！
+        //（STEP.1）倉庫を作成
+        let savedata = NSUserDefaults.standardUserDefaults()
+        //（STEP.2）書き込み
+        savedata.setObject(userNameTextField.text, forKey: "userName")
+        savedata.setObject(userSlider.value, forKey: "slider")
+        savedata.setObject(onoffSwitch.on, forKey: "onoff")
+        //（STEP.3）書き込みを確実にする！
+        savedata.synchronize()
+        
+        //次の画面に遷移する
+        performSegueWithIdentifier("toSecondView",sender: nil)
+    }
 
 }
 
